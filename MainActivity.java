740f2432c2dc4ccf6bf5c66b445eac61e8cf6d43
Ricardo3100 <38477282+Ricardo3100@users.noreply.github.com>
2018-09-15 20:06:08 -0700

    Linked the contact us button to the main screen,

---
+++ MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ricardo.fosterleaders;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public void gotocontactus(View view) {
+        Intent intent = new Intent(this, contactus.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
